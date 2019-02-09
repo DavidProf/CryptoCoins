9dfac68f308ef6548379bca43dc6abe9adfb1a8e
David Lima <david.lima50@etec.sp.gov.br>
2019-02-09 21:46:32 -0200

    App don't open

--- app/src/main/java/com/learn/dntl/cryptocoins/App/MainActivity.java
+++ app/src/main/java/com/learn/dntl/cryptocoins/App/MainActivity.java
@@ -2,14 +2,58 @@ package com.learn.dntl.cryptocoins.App;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.learn.dntl.cryptocoins.Model.CryptoCoin;
 import com.learn.dntl.cryptocoins.R;
+import com.learn.dntl.cryptocoins.RecyclerAdapter;
+import com.learn.dntl.cryptocoins.Service.APIService;
+import com.learn.dntl.cryptocoins.Service.RestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    APIService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        apiService = RestClient.getSource();
+
+        final ArrayList<CryptoCoin> cryptoCoins = new ArrayList<>();
+
+        apiService.getCryptoCoins().enqueue(new Callback<List<CryptoCoin>>() {//erro
+            @Override
+            public void onResponse(Call<List<CryptoCoin>> call, Response<List<CryptoCoin>> response) {
+                cryptoCoins.addAll(response.body());
+                recyclerView.setAdapter(new RecyclerAdapter(cryptoCoins));
+            }
+
+            @Override
+            public void onFailure(Call<List<CryptoCoin>> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+        LinearLayoutManager linearLayout = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(linearLayout);
     }
 }
---
+++ app/src/main/java/com/learn/dntl/cryptocoins/RecyclerAdapter.java
@@ -0,0 +1,57 @@
+package com.learn.dntl.cryptocoins;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.learn.dntl.cryptocoins.Model.CryptoCoin;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    ArrayList<CryptoCoin> cryptoCoins;
+
+    public RecyclerAdapter(ArrayList<CryptoCoin> cryptoCoins) {
+        this.cryptoCoins = cryptoCoins;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        CryptoCoin cryptoCoin = cryptoCoins.get(i);
+
+        viewHolder.name.setText(cryptoCoin.getName());
+        viewHolder.price.setText(cryptoCoin.getPriceUsd());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cryptoCoins.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.text_view_value_name)
+        TextView name;
+        @BindView(R.id.text_view_value_price)
+        TextView price;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
